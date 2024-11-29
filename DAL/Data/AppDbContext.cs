4ddfe3664a8347cb3baccd7816e0f67e218c59a7
Бек Аскер <2004bek.asker@mail.ru>
2024-11-29 21:24:00 +0500

    Добавьте файлы проекта.

---
+++ DAL/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<MenuItem> MenuItems { get; set; }
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,21 @@
+using DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.Run();
