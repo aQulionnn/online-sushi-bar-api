1a669df638a30467aac39af64b60c498afad8b2c
Бек Аскер <2004bek.asker@mail.ru>
2025-01-05 19:46:27 +0500

    feat: Add SortingParameters with validation for sorting criteria

---
+++ Application/Validators/MenuItem/SortingMenuItemsValidator.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Parameters;
+using FluentValidation;
+
+namespace Application.Validators.MenuItem
+{
+    public class SortingMenuItemsValidator : AbstractValidator<SortingParameters>
+    {
+        public SortingMenuItemsValidator()
+        {
+            RuleFor(x => x)
+                .Custom((sortingParameters, context) =>
+                {
+                    if (sortingParameters.ByPrice && sortingParameters.ByName)
+                        context.AddFailure("You can only sort by one criteria: ByName or ByPrice.");
+                });
+        }
+    }
+}
---
+++ DAL/Parameters/SortingParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Parameters
+{
+    public class SortingParameters
+    {
+        public bool ByName { get; set; }
+        public bool ByPrice { get; set; } 
+        public bool IsAscending { get; set; }
+    }
+}
