1152d8d22ebcd1c3b9a0eef36ca0942c04e28847
Бек Аскер <2004bek.asker@mail.ru>
2025-01-07 15:46:12 +0500

    feat: Implement caching for sorted MenuItems retrieval

--- Application/Features/MenuItem/Queries/GetSortedMenuItemsQueryHandler.cs
+++ Application/Features/MenuItem/Queries/GetSortedMenuItemsQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using BLL.Dtos.MenuItem;
+﻿using Application.Interfaces;
+using BLL.Dtos.MenuItem;
 using BLL.Interfaces;
 using DAL.Parameters;
 using DAL.SharedKernels;
@@ -9,15 +10,23 @@ namespace Application.Features.MenuItem.Queries
     public class GetSortedMenuItemsQueryHandler : IRequestHandler<GetSortedMenuItemsQuery, Result<IEnumerable<GetMenuItemDto>>>
     {
         private readonly IMenuItemService _menuItemService;
+        private readonly IRedisService _redisService;
 
-        public GetSortedMenuItemsQueryHandler(IMenuItemService menuItemService)
+        public GetSortedMenuItemsQueryHandler(IMenuItemService menuItemService, IRedisService redisService)
         {
             _menuItemService = menuItemService;
+            _redisService = redisService;
         }
 
         public async Task<Result<IEnumerable<GetMenuItemDto>>> Handle(GetSortedMenuItemsQuery request, CancellationToken cancellationToken)
         {
+            var cachedMenuItems = await _redisService.GetDataAsync<IEnumerable<GetMenuItemDto>>($"menuItems:sorted:{request.Sorting.GetHashCode()}");
+            if (cachedMenuItems != null)
+                return Result<IEnumerable<GetMenuItemDto>>.Success(cachedMenuItems);
+
             var menuItems = await _menuItemService.GetAllWithSortingAsync(request.Sorting);
+            await _redisService.SetDataAsync($"menuItems:sorted:{request.Sorting.GetHashCode()}", menuItems);
+
             return Result<IEnumerable<GetMenuItemDto>>.Success(menuItems);
         }
     }
